7f91f297e76457e5af974d73239aad9410dc0d9c
Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>
2025-07-29 23:12:53 +0200

    DEBUG

--- tools/auto-qwt/install-qwt.bat
+++ tools/auto-qwt/install-qwt.bat
@@ -4,6 +4,10 @@ title %~f0
 :: Copyright (C) 2023 Elliot Killick <contact@elliotkillick.com>
 :: Licensed under the MIT License. See LICENSE file for details.
 
+:: DEBUG
+
+reg add "HKLM\Software\Invisible Things Lab\Qubes Tools" /v "LogLevel" /t REG_DWORD /d 5
+
 cd installer || exit
 for %%i in (qubes-tools-*.exe qubes-tools-*.msi) do (
     start %%i /passive
